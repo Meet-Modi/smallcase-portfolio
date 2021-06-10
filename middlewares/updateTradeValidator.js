c1569ca4a970736b970119ba78a8b145ccb47d2b
meet <meetmodi8@gmail.com>
2021-06-10 15:31:46 +0530

    validate check

--- middlewares/updateTradeValidator.js
+++ middlewares/updateTradeValidator.js
@@ -2,12 +2,13 @@
 // of price and if BUY AND SELL is only passed in the parameters. If not true, will reject the user 
 // request.
 const checkUpdateRequest = async function(req,res,next){
+    
     var TradeData = req.body;
-    if(Number.isInteger(Tradedata.tradeId)){
-        if(data.tradeType == "Buy" || data.tradeType == "Sell"){
-            if(Number.isInteger(data.quantity)){
-                if(typeof(data.unitPrice)=='number'){
-                    ext();
+    if(Number.isInteger(TradeData.tradeId)){
+        if(TradeData.tradeType == "Buy" || TradeData.tradeType == "Sell"){
+            if(Number.isInteger(TradeData.Quantity)){
+                if(typeof(TradeData.unitPrice)=='number'){
+                    next();
                 }
                 else
                     res.status(500).send({message : "Request rejected. Possible reasons can be lowercase tickersymbol or quantity,tradeUd is not an integer or unitprice is not a number"})
@@ -16,11 +17,12 @@ const checkUpdateRequest = async function(req,res,next){
             res.status(500).send({message : "Request rejected. Possible reasons can be lowercase tickersymbol or quantity,tradeUd is not an integer or unitprice is not a number"})
         }
         else
-        res.status(500).send({message : "Request rejected. Possible reasons can be lowercase tickersymbol or quantity,tradeUd is not an integer or unitprice is not a number"})
+        res.status(500).send({message : "Request rejected. Possible reasons can be lowercase tickersymbol or quantity,tradeId is not an integer or unitprice is not a number"})
     }
     else
     res.status(500).send({message : "Request rejected. Possible reasons can be lowercase tickersymbol or quantity,tradeUd is not an integer or unitprice is not a number"})
-
+    
+   //next();
 }
 
 module.exports = {
